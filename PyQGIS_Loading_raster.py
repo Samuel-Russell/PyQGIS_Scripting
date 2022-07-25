c647732b3d928bbf708e3f2d931f37ae8b5979f9
Samuel Russell <69426323+Samuel-Russell@users.noreply.github.com>
2022-07-25 05:43:33 +0200

    Update PyQGIS_Loading_raster.py
    Updated script to include getting raster information

--- PyQGIS_Loading_raster.py
+++ PyQGIS_Loading_raster.py
@@ -7,3 +7,17 @@ DEM_Raster_1.isValid()
 
 # Visualise Raster
 QgsProject.instance().addMapLayer(DEM_Raster_1)
+
+# Inspecting raster values
+## Raster width:
+
+print(DEM_Raster_1.height(), ':', DEM_Raster_1.width())
+
+## Raster extent
+
+print(DEM_Raster_1.extent())
+
+## Bounding box coordinates
+
+ext = DEM_Raster_1.extent()
+print(ext.xMinimum(), ext.yMinimum(), ':', ext.xMaximum(), ext.yMaximum())
